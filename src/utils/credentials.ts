61e3588a7a4146fefa0a8f99d2713d713edf9285
shauneang <shauneangjx@gmail.com>
2025-03-13 15:33:32 +0800

    Remove Netlify

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -7,18 +7,12 @@ dotenv.config(); // Load .env variables in local development
 export function createTempGoogleCredentials() {
     const credentialsPath = path.join('/tmp', 'gcp-credentials.json'); // Works on Vercel
 
-    // if (!process.env.GOOGLE_CREDENTIALS_JSON) {
-    //     throw new Error("GOOGLE_CREDENTIALS_JSON is not set");
-    // }
-    if (!process.env.GOOGLE_CREDENTIALS_BASE64) {
-        throw new Error("GOOGLE_CREDENTIALS_BASE64 is not set");
+    if (!process.env.GOOGLE_CREDENTIALS_JSON) {
+        throw new Error("GOOGLE_CREDENTIALS_JSON is not set");
     }
 
     // Write JSON credentials to a temp file
-    const decodedCredentials = Buffer.from(process.env.GOOGLE_CREDENTIALS_BASE64, "base64").toString("utf8");
-    fs.writeFileSync(credentialsPath, decodedCredentials, "utf8");
-
-    // fs.writeFileSync(credentialsPath, process.env.GOOGLE_CREDENTIALS_JSON);
+    fs.writeFileSync(credentialsPath, process.env.GOOGLE_CREDENTIALS_JSON);
 
     // Set the environment variable dynamically
     process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
@@ -28,18 +22,12 @@ export function createTempGoogleCredentials() {
 export function createTempFirebaseCredentials() {
     const credentialsPath = path.join('/tmp', 'firebase-credentials.json'); // Works on Vercel
 
-    // if (!process.env.FIREBASE_CREDENTIALS_JSON) {
-    //     throw new Error("FIREBASE_CREDENTIALS_JSON is not set");
-    // }
-    if (!process.env.FIREBASE_CREDENTIALS_BASE64) {
-        throw new Error("FIREBASE_CREDENTIALS_BASE64 is not set");
+    if (!process.env.FIREBASE_CREDENTIALS_JSON) {
+        throw new Error("FIREBASE_CREDENTIALS_JSON is not set");
     }
 
     // Write JSON credentials to a temp file
-    const decodedCredentials = Buffer.from(process.env.FIREBASE_CREDENTIALS_BASE64, "base64").toString("utf8");
-    fs.writeFileSync(credentialsPath, decodedCredentials, "utf8");
-
-    // fs.writeFileSync(credentialsPath, process.env.FIREBASE_CREDENTIALS_JSON);
+    fs.writeFileSync(credentialsPath, process.env.FIREBASE_CREDENTIALS_JSON);
 
     // Set the environment variable dynamically
     process.env.FIREBASE_CREDENTIALS = credentialsPath;
